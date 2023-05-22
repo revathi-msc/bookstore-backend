898bb96c9303468d0bed7f5f4f27605252fbdd29
Revathi Mahalingam <103559154+revathimsc@users.noreply.github.com>
2023-05-22 16:44:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,11 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cors());
-app.get("/", (req,res) => {
-res.setHeader("Access-Control-Allow-Credentials","true");
-res.send("hello");
-})
-
 app.use("/books", router); // localhost:5000/books
- 
 
 mongoose
   .connect(
-    "mongodb+srv://admin:admin@cluster0.uro3q2f.mongodb.net/bookstore?retryWrites=true&w=majority"
+    "mongodb+srv://bookstore:bookstore@cluster0.dybxewr.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => console.log("Connected To Database"))
   .then(() => {
